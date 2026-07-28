3325aef1b23aa7d293c5758d7668df225f0f75bc
Max Diamandopol <diamandopol.andrei10@gmail.com>
2026-07-28 18:06:40 +0100

    Ruff lsp to use .toml files for linting rules

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,15 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" , "pyright", "ruff", "svelte"}
+vim.lsp.config("ruff", {
+  init_options = {
+    settings = {
+      -- Prefer pyproject.toml over editor-defined Ruff settings.
+      configurationPreference = "filesystemFirst",
+    },
+  },
+})
+
+local servers = { "html", "cssls", "pyright", "ruff", "svelte" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
